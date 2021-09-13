c3ef9b716431889c0f81927e6ee3653539dda711
Exordio <wecatorz@gmail.com>
2021-09-13 03:26:29 +0300

    edits

--- clear-rabbit.sh
+++ clear-rabbit.sh
@@ -6,4 +6,5 @@ mkdir data etc logs
 chmod 777 data
 chmod 777 etc/
 chmod 777 logs
-docker-compose up -d && docker-compose logs -f -t
+docker-compose up -d && docker exec docker-compose-phpfpm-nginx-mysql-rabbit_rabbitmq_1 rabbitmq-plugins enable rabbitmq_management
+docker-compose logs -f -t
